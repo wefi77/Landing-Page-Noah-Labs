9b950ff331875cdfcd18428121f8d143a1204e4a
wefi77 <kmendo111@gmail.com>
2026-07-16 22:50:29 +0700

    Initial commit

---
+++ contact-links.js
@@ -0,0 +1,4 @@
+document.querySelectorAll('.contact-menu a').forEach(link=>{
+  if(link.textContent.includes('WhatsApp')) link.href='https://chat.whatsapp.com/GP4G4sLdV9GDeqhKVVCiXL';
+  if(link.textContent.includes('Telegram')) link.href='https://telegram.me/+_ahtSR5MNrQ3ZDk1';
+});
